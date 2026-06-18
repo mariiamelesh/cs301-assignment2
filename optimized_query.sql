142e5fe1eec011a233aee55f3bc4d0688f698bd0
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-18 14:59:51 +0300

    filtered users with bad reviews only

--- optimized_query.sql
+++ optimized_query.sql
@@ -1,2 +1,9 @@
-select user_id, country
-from users
+with bad_reviews as (
+	select user_id, rating
+	from reviews
+	where rating < 3
+)
+
+select br.user_id, br.rating, users.country
+from bad_reviews br
+join users on br.user_id = users.user_id
