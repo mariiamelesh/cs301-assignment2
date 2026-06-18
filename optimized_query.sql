fc6ea129258f0cbaec1c8e3cacf1de2ebc260b46
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-18 15:10:57 +0300

    added simple aliases; added lifetime_Value cte

--- optimized_query.sql
+++ optimized_query.sql
@@ -1,9 +1,15 @@
 with bad_reviews as (
-	select user_id, rating
-	from reviews
+	select r.user_id, country, product_name
+	from reviews r
+	join users u on r.user_id = u.user_id
+	join products p on r.product_id = p.product_id
 	where rating < 3
+), lifetime_value as (
+	select user_id, sum(total_amount) as lifetime_value
+	from purchases
+	group by user_id
 )
 
-select br.user_id, br.rating, users.country
+select br.user_id, br.country, br.product_name, ltv.lifetime_value
 from bad_reviews br
-join users on br.user_id = users.user_id
+join lifetime_value ltv on br.user_id = ltv.user_id
