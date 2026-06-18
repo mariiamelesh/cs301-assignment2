4599f582195d31ba489f4ff1d971233e10fe0e26
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-18 16:25:11 +0300

    added long_view cte

--- optimized_query.sql
+++ optimized_query.sql
@@ -1,15 +1,28 @@
-with bad_reviews as (
-	select r.user_id, country, product_name
+with premium_products as (
+	select product_id, product_name
+	from products
+	where product_name like '%Pro%' or product_name like '%Max%'
+), bad_reviews as (
+	select distinct r.user_id, pp.product_id
 	from reviews r
-	join users u on r.user_id = u.user_id
-	join products p on r.product_id = p.product_id
-	where rating < 3
+	join premium_products pp on r.product_id = pp.product_id
+	where rating <= 2
+), long_view as ( 
+	select br.user_id, br.product_id
+	from bad_reviews br
+	join interactions i on br.user_id = i.user_id and br.product_id = i.product_id
+	where i.interaction_type = 'view'
+	group by br.user_id, br.product_id
+	having sum(i.dwell_time_ms) > 60000
 ), lifetime_value as (
-	select user_id, sum(total_amount) as lifetime_value
-	from purchases
-	group by user_id
+	select lv.user_id, sum(p.total_amount) as lifetime_value
+	from long_view lv
+	join purchases p on lv.user_id = p.user_id
+	group by lv.user_id
 )
 
-select br.user_id, br.country, br.product_name, ltv.lifetime_value
-from bad_reviews br
-join lifetime_value ltv on br.user_id = ltv.user_id
+select u.user_id, u.country, p.product_name, ltv.lifetime_value
+from lifetime_value ltv
+join users u on ltv.user_id = u.user_id
+join long_view lv on ltv.user_id = lv.user_id
+join products p on lv.product_id = p.product_id
